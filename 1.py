e9d4876f8731d51a585260f81210beb3ebad74c9
hellojunho <jo62698476@gmail.com>
2022-09-26 15:01:02 +0900

    220926 commit

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
-print("hello world")
-print("hellojunho/vsCode")
+# print("hello world")
+# print("hellojunho/vsCode")
 
-for i in range(1,10):
-    print(i, end=" ")
+# for i in range(1,10):
+#     print(i, end=" ")
 
-print("깃허브 연동하기 어려워!")
+# print("깃허브 연동하기 어려워!")
